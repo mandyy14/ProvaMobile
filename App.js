8f4663b4d60a51c8666f9392ecf8aad21883efb1
amanda <amandamesquita73638@gmail.com>
2025-03-14 20:12:12 -0300

    updated app js

--- App.js
+++ App.js
@@ -1,19 +1,19 @@
 import { NavigationContainer } from "@react-navigation/native";
 import { createStackNavigator } from "@react-navigation/stack";
 import Forms from "./src/component/forms";
+import TelaResultado from "./src/screen/resultado";
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
+
     <NavigationContainer>
-      <Stack.Navigator>
-        <Stack.Screen
-          name="Forms"
-          component={Forms}
-          options={{ headerShown: false }}
-        />
+      <Stack.Navigator initialRouteName="Forms">
+        <Stack.Screen name="Forms" component={Forms} options={{ title: 'Forms' }} />
+        <Stack.Screen name="Resultado" component={TelaResultado} options={{ title: 'Resultado' }} />
       </Stack.Navigator>
     </NavigationContainer>
   );
+
 }
--- src/screen/resultado.jsx
+++ src/screen/resultado.jsx
@@ -19,4 +19,5 @@ const styles = StyleSheet.create({
         alignItems: "center",
         justifyContent: "center",
     },
+
 });
